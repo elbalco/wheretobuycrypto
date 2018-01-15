787c7531226df4359194be08bb16d58ed2c28d6f
victormier <victormier@gmail.com>
2018-01-15 17:04:04 +0100

    Add search class method to Coin

--- app/models/coin.rb
+++ app/models/coin.rb
@@ -1,4 +1,9 @@
 class Coin < ActiveRecord::Base
   has_many :coin_exchanges
   has_many :exchanges, through: :coin_exchanges
+
+  def self.search(param)
+    Coin.where("key ILIKE ? OR name ILIKE ? OR symbol ILIKE ?", "%#{param}%","%#{param}%","%#{param}%")
+  end
+
 end
