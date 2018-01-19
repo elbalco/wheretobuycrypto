3ad5b3080adfaf65a73388fa142542e18ea44355
victormier <victormier@gmail.com>
2018-01-19 16:32:41 +0100

    Add referral url on serialization

--- app/models/exchange.rb
+++ app/models/exchange.rb
@@ -3,4 +3,8 @@ class Exchange < ActiveRecord::Base
   has_many :coins, through: :coin_exchanges
 
   scope :ordered, -> { order("volume_24h desc") }
+
+  def url
+    referral_url || super
+  end
 end
