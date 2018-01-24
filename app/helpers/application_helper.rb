91a9341891845384c001a1ef030990a2537530c8
Roc Toll Pifarre <roctoll@MacBook-Pro-de-Roc.local>
2018-01-24 11:32:38 +0100

    shorten meta description

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,7 +9,7 @@ module ApplicationHelper
     if @coin
       "Find all exchanges where #{@coin.name}(#{@coin.symbol}) cryptocurrency can be bought or exchanged. Easiest tool to find which exchange you can buy any cryptocurrency."
     else
-      "Easiest tool to find which exchange you can buy any cryptocurrency. We have 1500+ cryptocurrencies listed. List of trusted exchanges where you can buy Bitcoin, Ethereum, Ripple, Bitcoin Cash, Cardano, Litecoin, NEM, NEO, Stellar, IOTA, Icon, Wanchain, ...and 1500 more."
+      "Easiest tool to find which exchange you can buy any cryptocurrency. We have 1500+ cryptocurrencies listed. Only trusted trusted exchanges updated in real-time"
     end
   end
 end
