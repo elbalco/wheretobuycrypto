192ca40ebd0b0f0518efc96b6e6eaa197f13deda
victormier <victormier@gmail.com>
2018-01-30 12:32:41 -0500

    Move lib to /app

--- config/application.rb
+++ config/application.rb
@@ -6,14 +6,11 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-require './lib/twitter_client'
-
 module Wheretobuycrypto
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
     config.autoload_paths += %W( #{config.root}/app/services #{config.root}/app/workers #{config.root}/lib )
-    config.eager_load_paths += %W( #{config.root}/lib )
   end
 end
