382b53f93d024e5163b1017ac400cf7bcf3b5199
victormier <victormier@gmail.com>
2018-01-30 12:24:18 -0500

    Try eager loading lib dir

--- config/application.rb
+++ config/application.rb
@@ -12,5 +12,6 @@ module Wheretobuycrypto
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
     config.autoload_paths += %W( #{config.root}/app/services #{config.root}/app/workers #{config.root}/lib )
+    config.eager_load_paths += %W( #{config.root}/lib )
   end
 end
